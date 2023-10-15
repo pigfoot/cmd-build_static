0754ed6b5a820ab2917ac316e5d07c5d3eb9f367
Chen, Chih-Chia <pigfoot@gmail.com>
2023-10-15 17:51:24 +0800

    [ffmpeg] refine build script
    Signed-off-by: Chen, Chih-Chia <pigfoot@gmail.com>

--- ffmpeg/run-debian.sh
+++ ffmpeg/run-debian.sh
@@ -3,10 +3,10 @@
 set -ex
 
 builder=$(buildah from "docker.io/library/debian:latest")
-buildah config --workingdir '/io' "${builder}"
-buildah run "${builder}" sh -c 'echo "export DEBIAN_FRONTEND=noninteractive\nexport TERM=xterm-256color" >> ~/.bashrc'
+buildah config --workingdir '/io' --env DEBIAN_FRONTEND="noninteractive" --env TERM="xterm-256color" "${builder}"
+buildah run "${builder}" sh -c 'echo "export TERM=xterm-256color" >> ~/.bashrc'
 buildah run "${builder}" sh -c 'echo "deb http://deb.debian.org/debian $(sed -En "/^VERSION_CODENAME/ s#.*=##p" /etc/os-release) contrib non-free non-free-firmware" > /etc/apt/sources.list'
-buildah run "${builder}" sh -c 'apt update -qq && apt upgrade -qq -y'
+buildah run "${builder}" sh -c 'apt update -qq && apt upgrade -qq -y && apt install -qq -y apt-utils'
 buildah run "${builder}" sh -c 'apt install -qq -y build-essential cmake curl git \
     libfdk-aac-dev libmp3lame-dev libopus-dev libssl-dev libswscale-dev \
     libvorbis-dev libx264-dev libnuma-dev libass-dev zlib1g-dev \
--- ffmpeg/run-ubuntu.sh
+++ ffmpeg/run-ubuntu.sh
@@ -3,9 +3,9 @@
 set -ex
 
 builder=$(buildah from "docker.io/library/ubuntu:rolling")
-buildah config --workingdir '/io' "${builder}"
-buildah run "${builder}" sh -c 'sed -Ei "/[ -z \"$PS1\" ] && return/aexport DEBIAN_FRONTEND=noninteractive\nexport TERM=xterm-256color" ~/.bashrc'
-buildah run "${builder}" sh -c 'apt update -qq && apt upgrade -qq -y'
+buildah config --workingdir '/io' --env DEBIAN_FRONTEND="noninteractive" --env TERM="xterm-256color" "${builder}"
+buildah run "${builder}" sh -c 'sed -Ei "/[ -z \"$PS1\" ] && return/aexport TERM=xterm-256color" ~/.bashrc'
+buildah run "${builder}" sh -c 'apt update -qq && apt upgrade -qq -y && apt install -qq -y apt-utils'
 buildah run "${builder}" sh -c 'apt install -qq -y build-essential cmake curl git \
     libfdk-aac-dev libmp3lame-dev libopus-dev libssl-dev libswscale-dev \
     libvorbis-dev libx264-dev libnuma-dev libass-dev libz-dev \
